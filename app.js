4edb821cf331b328ceebf94e65b3fa39cde3b25f
SofieTornqvist <sofietornqvist@hotmail.com>
2025-10-22 10:35:14 +0200

    Lagt till övning från TH

---
+++ app.js
@@ -0,0 +1,10 @@
+let firstName = "Sofie" 
+const homeTown = "Lund"
+
+
+console.log(firstName)
+console.log("firstName är datatyp:", typeof firstName)
+console.log(homeTown) 
+console.log("homeTown är datatyp:", typeof homeTown)
+
+
